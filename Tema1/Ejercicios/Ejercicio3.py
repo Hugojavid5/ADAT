f7ce8cc3300093ab353fea717190cf9f4e9304a9
Hugo <hugojavidferrero@gmail.com>
2024-09-19 08:58:09 +0200

    Subida de ejercicio3.py

--- Tema1/Ejercicios/Ejercicio3.py
+++ Tema1/Ejercicios/Ejercicio3.py
@@ -1,2 +1,14 @@
 ##Modifica el programa anterior de forma que la lista de 10 números sean números impares.
 ##(hay que asegurarse de que lo que se introduce en la lista son números).
+numeros = []
+for i in range(10):
+    numero = int(input(f"Ingrese el numero impar {i+1}: "))
+    if numero %2 !=0:
+        numeros.append(numero)
+    else:
+        print("El numero no es impar.Introduce uno impar")
+print("Lista de numeros impares:",numeros)
+suma=sum(numeros)
+print("el sumatorio es:",suma)
+med=suma/len(numeros)
+print("la media de la lista es:",med)
